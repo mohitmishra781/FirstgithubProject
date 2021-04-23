ef1af67fa0d123bfede7a0321d293545ba12a87c
mohit <mohit@LAPTOP-BT67K4HR>
2021-04-23 13:40:26 +0530

    Class created

---
+++ Thirdproject/src/test/java/firstpack/Secondclass.java
@@ -0,0 +1,16 @@
+package firstpack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Secondclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver d=new ChromeDriver();
+				d.get("https://www,gmail.com");
+
+	}
+
+}
